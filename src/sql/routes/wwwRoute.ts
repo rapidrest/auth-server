98dbc8e9860c746fd909acd126883c94c180063d
Jean-Philippe Steinmetz <caskater47@gmail.com>
2026-07-19 22:09:44 -0700

    Adding default routes

---
+++ src/sql/routes/wwwRoute.ts
@@ -0,0 +1,13 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2026 Jean-Philippe Steinmetz
+///////////////////////////////////////////////////////////////////////////////
+import { ReactRoute } from "@rapidrest/react";
+import { RouteDecorators } from "@rapidrest/service-core";
+
+const { ApiRoute } = RouteDecorators;
+
+@ApiRoute("/")
+export class AppRoute extends ReactRoute {
+    protected readonly appDir: string = "apps/www";
+    protected readonly hydrate: boolean = true;
+}
