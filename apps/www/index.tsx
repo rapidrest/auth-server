ade4c0c3162e4de48f81dc8097e69c9838620d7d
Jean-Philippe Steinmetz <caskater47@gmail.com>
2026-08-03 19:14:23 -0700

    Upgrading @rapidrest/react to v0.10.0

--- apps/www/index.tsx
+++ apps/www/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function HomePage() {
-    return <p>Welcome to RapidREST React!</p>;
+    return <p>Welcome to RapidREST Auth Server!</p>;
 }
 
 export async function fetchProps() {
